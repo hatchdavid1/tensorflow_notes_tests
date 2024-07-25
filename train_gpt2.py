73a85e3f4124d2eb2d9235193bbddbe18559fbb0
David Hatch <51379112+hatchdavid1@users.noreply.github.com>
2024-07-25 11:37:36 -0600

    Add files via upload
    Causal Self Attention Class

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,6 +3,39 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F 
 
+class CausalSelfAttention(nn.Module): 
+
+    def __init__(self, config): 
+        super().__init__()
+        assert = config.n_embd % config.n_head == 0
+        # Key, query and valyue projections for all heads but in a batch 
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        # Output projection 
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        # Regularixzation 
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size)
+                                    .view(1,1, config.block_size, config.block_size)))
+
+
+    def forward(self, x): 
+        B, T, C = x.size() # Batch size, sequence length, embedding dimensionality (n_embd)
+        # Calculate query, key and values for all heads in bathc and move head forward to batch
+        # nh is the number of heads, hs us the head size and c number of channels = nh * ns
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim = 2)
+        k = k.view(B, T, self.n_head, C // self.n_head).transpose(1,2)
+        q = q.view(B, T, self.n_head, C // self.n_head).transpose(1,2)
+        v = v.view(B, T, self.n_head, C // self.n_head).transpose(1,2)
+        # Attention mateializes the large (T,T) matrix for all queries and keys  
+        att = (q @ k.transpose(-2,-1)) * (1.0 / math.sqrt(k.size(-1)))
+        att = att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        att = F.softmax(att, dim = -1)
+        y = att @ v # (B, nh, T, T) x (B, nh, T, hs)
+        y = y.transpose(1,2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
 
 class MLP(nn.Module):
     
